c601891cb39fe664f8dd1e8fd7d9fbb0b92ab747
Dhafin Alghani <101612412+dhafin109@users.noreply.github.com>
2023-06-14 05:39:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import os
-import tensorflow
+import io
 import numpy as np
-from keras.models import load_model
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+import tensorflow as tf
+from keras.layers import Input, Embedding, Flatten, Dot, Dense, Concatenate
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from sklearn.preprocessing import LabelEncoder
 import requests
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Load recommendation_rating_model.h5
-model = load_model("recommendation_rating_model.h5")
+model = tf.keras.models.load_model("recommendation_rating_model.h5")
 # Label yang ada di dalam recommendation_rating_model.h5
-label = ["Epochs", "Training Error"]
+label = ["Epochs","Training Error"]
 
-@app.route("/predict", methods=["GET", "POST"])
-def index():
-    file = request.files.get('file')
-    if file is None or file.filename == "":
-        return jsonify({"error":"no file"})
-    
-    # Call the predict function with the user id from the file
-    user_id = 1 # Replace this with the user id from the file
-    response_json = predict(user_id)
-    
-    return jsonify(response_json)
+app = Flask(__name__)
 
+@app.route("recommendation_rating_model.h5")
+def hello():
+    """Returns a greeting"""
+    return "Hello, World!"
+
+@app.route("/recommendation_rating_model.h5/predict/<int:id>", methods=["GET","POST"])
 def predict(id):
     """Makes a prediction for a user and returns recommended places"""
     id_place = range(1, 20)
@@ -40,7 +45,7 @@ def predict(id):
 
     predictions = np.array([a[0] for a in predictions])
 
-    recommended_tourism_ids = predictions.argsort()[:10]
+    recommended_tourism_ids = (predictions).argsort()[:10]
     list_recommended_tourism_ids = recommended_tourism_ids.tolist()
 
     # get data from api to convert id to place id
@@ -54,16 +59,14 @@ def predict(id):
     response_json = {
         "data": user_id,
         "prediction": place_id,
-        "category": "",
-        "city": "",
-        "description": "",
-        "img": "",
-        "lang": "",
-        "lat": "",
-        "name": ""
+        "category": " ",
+        "city": " ",
+        "description": " ",
+        "img": " ",
+        "lang": " ",
+        "lat": " ",
+        "name": " ",
     }
-    
-    return response_json
 
-if __name__ == "__main__":
-    app.run()
+    # return a response in json format
+    return jsonify(response_json), 200
